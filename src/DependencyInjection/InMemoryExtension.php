35add309b4fa5bffab794158348b16badc074487
TBoileau <t-boileau@email.com>
2021-11-12 19:44:06 +0100

    fix: add autoconfiguration for FixtureInterface

--- src/DependencyInjection/InMemoryExtension.php
+++ src/DependencyInjection/InMemoryExtension.php
@@ -8,6 +8,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use TBoileau\InMemoryBundle\DataFixtures\FixtureInterface;
 
 final class InMemoryExtension extends Extension
 {
@@ -15,5 +16,7 @@ final class InMemoryExtension extends Extension
     {
         $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.php');
+
+        $container->registerForAutoconfiguration(FixtureInterface::class)->addTag('app.in_memory.fixtures');
     }
 }
